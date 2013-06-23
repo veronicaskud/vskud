19c74178d1b4af47432d0b5dd41774864b0f33b5
Daniel Beck <daniel@danielbeck.net>
2013-06-23 16:47:01 -0400

    Rewriting to remove foundation grid, IE incompatibilities
    In progress.  Have top nav and hero working.

--- public/telling/website/js/frills.js
+++ public/telling/website/js/frills.js
@@ -1,24 +1,57 @@
 
-
-
 $(document).ready(function() {
-// cheap workaround for the top navbar jumping on initial scroll... not sure where that's coming from, too lazy to find it:
-	$(window).scrollTop(1);$(window).scrollTop(0);
+	$('.toggle-topbar').click(function() {
+		$('.top-nav').toggleClass('expanded');
+	});
+	
+	
+	$(window).resize(resizeHandler);
+	$(window).scroll(scrollHandler);
 
 
-if (! (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent))) {
-		parallax()
-}
+	
+	if (! (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent))) {
+		parallax();
+	}
 
 }); 
 
 
+
+
+
+
+
+var resizeHandler = function() {
+
+}
+
+var scrollHandler = function() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function parallax() {
-	$('.heroimage, .footer').css("position","fixed");
+	$('.herocontainer, .footer').css("position","fixed");
 
 //	var footertop = $(document).height();
 	$('body').css("marginBottom",$('.footer').height());
-
+	$(window).scrollTop(1);
 	
 // even cheaper workaround to reset footer position on window resize:
 $(window).resize(function() {$(window).scrollTop($(window).scrollTop()-1)});
@@ -26,12 +59,12 @@ $(window).resize(function() {$(window).scrollTop($(window).scrollTop()-1)});
 
 $(window).scroll(function() {
 		//parallax scroll for footer
-		var distFromBottom = $(document).height() - ($(window).scrollTop() + $(window).height());
-		$('.footer').css("top",($('footer').position().top-$(window).scrollTop())+"px");
-		$('.footer').css("backgroundPosition","50% -"+(distFromBottom/2)+"px");
+// 		var distFromBottom = $(document).height() - ($(window).scrollTop() + $(window).height());
+// 		$('.footer').css("top",($('footer').position().top-$(window).scrollTop())+"px");
+// 		$('.footer').css("backgroundPosition","50% -"+(distFromBottom/2)+"px");
 		
 		// and for hero:
-		$('.heroimage').css("top","-"+($(window).scrollTop())+"px");
+		$('.herocontainer').css("top","-"+($(window).scrollTop())+"px");
 		$('.heroimage').css("backgroundPosition","50% "+($(window).scrollTop()/2)+"px");
 		
 	});

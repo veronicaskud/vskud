b8f0d73cb6d05659db93ff812a3a031946fefcf6
Daniel Beck <daniel@danielbeck.net>
2013-06-23 00:03:05 -0400

    Unjittered parallax
    Couple of hideous hacks in frills.js, clean up someday when I'm smarter

--- public/telling/website/js/frills.js
+++ public/telling/website/js/frills.js
@@ -1,13 +1,29 @@
+$(document).ready(function() {
+
+	$('.heroimage, .footer').css("position","fixed");
+
+//	var footertop = $(document).height();
+	$('body').css("marginBottom",$('.footer').height());
+
 // cheap workaround for the top navbar jumping on initial scroll... not sure where that's coming from, too lazy to find it:
-$(document).ready(function() {$(window).scrollTop(1);$(window).scrollTop(0)}); 
+	$(window).scrollTop(1);$(window).scrollTop(0);
+	
+// even cheaper workaround to reset footer position on window resize:
+$(window).resize(function() {$(window).scrollTop($(window).scrollTop()-1)});
+
 
 $(window).scroll(function() {
 		//parallax scroll for footer
 		var distFromBottom = $(document).height() - ($(window).scrollTop() + $(window).height());
+		$('.footer').css("top",($('footer').position().top-$(window).scrollTop())+"px");
 		$('.footer').css("backgroundPosition","50% -"+(distFromBottom/2)+"px");
 		
 		// and for hero:
+		$('.heroimage').css("top","-"+($(window).scrollTop())+"px");
 		$('.heroimage').css("backgroundPosition","50% "+($(window).scrollTop()/2)+"px");
 		
-		
 	});
+
+
+}); 
+
